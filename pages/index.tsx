51c87da257f31d99ade6a0a7beec3f68ae8299d4
Sadri <108393507+fragmetal@users.noreply.github.com>
2024-10-06 14:23:59 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -74,18 +74,9 @@ export default function Home({
           textAlign: 'center'
         }}>
           Open-source monitoring and status page powered by{' '}
-          <a href="https://github.com/lyc8503/UptimeFlare" target="_blank">
-            Uptimeflare
-          </a>{' '}
-          and{' '}
           <a href="https://www.cloudflare.com/" target="_blank">
-            Cloudflare
+            Cloudflare.
           </a>
-          , made with ❤ by{' '}
-          <a href="https://github.com/lyc8503" target="_blank">
-            lyc8503
-          </a>
-          .
         </Text>
       </main>
     </>
