0d4d6b5158ad62b2db089efb976e8cb79a30ea5f
Sadri <108393507+fragmetal@users.noreply.github.com>
2024-10-06 14:45:37 +0800

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,24 +4,24 @@ import { pageConfig } from '@/uptime.config'
 
 export default function Header() {
   const linkToElement = (link: { label: string; link: string; highlight?: boolean }) => {
-    return (
+  return (
+    <Link href={link.link} passHref> {/* Use Link instead of a */}
       <a
         key={link.label}
-        href={link.link}
-        target="_blank"
         className={classes.link}
         data-active={link.highlight}
       >
         {link.label}
       </a>
-    )
-  }
+    </Link>
+  )
+}
 
   return (
     <header className={classes.header}>
       <Container size="md" className={classes.inner}>
         <div>
-          <a href="/">
+          <Link href="/"> {/* Use Link instead of a */}
             <Text size="xl" span>
               🕒
             </Text>
@@ -34,15 +34,27 @@ export default function Header() {
             >
               UptimeFlare
             </Text>
-          </a>
+          </Link>
         </div>
-
+  
         <Group gap={5} visibleFrom="sm">
-          {pageConfig.links.map(linkToElement)}
+          {pageConfig.links.map(link => (
+            <Link key={link.label} href={link.link} passHref> {/* Use Link instead of a */}
+              <a className={classes.link} data-active={link.highlight}>
+                {link.label}
+              </a>
+            </Link>
+          ))}
         </Group>
-
+  
         <Group gap={5} hiddenFrom="sm">
-          {pageConfig.links.filter((link) => (link as any).highlight).map(linkToElement)}
+          {pageConfig.links.filter((link) => (link as any).highlight).map(link => (
+            <Link key={link.label} href={link.link} passHref> {/* Use Link instead of a */}
+              <a className={classes.link} data-active={link.highlight}>
+                {link.label}
+              </a>
+            </Link>
+          ))}
         </Group>
       </Container>
     </header>
